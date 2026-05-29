c2fd2f6833854ba330e839bed408ac1c9d58ea55
eayzaid <aymanzaidanedev@gmail.com>
2026-05-30 00:38:35 +0100

    feat : we have now added the email service , so that the users can get their password communicated after their profile creation. tech-debt : we haven't yet test the endpoint for creating users

--- PlatformGateway/src/main/java/com/example/platformgateway/controller/UserController.java
+++ PlatformGateway/src/main/java/com/example/platformgateway/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.example.platformgateway.controller;
 
+import com.example.platformgateway.model.dto.CreateUserRequestDTO;
+import com.example.platformgateway.model.dto.CreateUserResponseDTO;
 import com.example.platformgateway.model.dto.UserSummaryResponseDTO;
 import com.example.platformgateway.model.enums.Role;
 import com.example.platformgateway.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +28,11 @@ public class UserController {
           @RequestParam(name = "role", required = false) Role role
   ){
     return ResponseEntity.ok(userService.getAllUsers(page,firstName,lastName,role));
+  }
+
+  @PostMapping
+  public ResponseEntity<CreateUserResponseDTO> createUser(@RequestBody CreateUserRequestDTO createUserRequestDTO){
+    return ResponseEntity.status(201).body(userService.createUser(createUserRequestDTO));
   }
 
 }
--- PlatformGateway/src/main/java/com/example/platformgateway/service/UserService.java
+++ PlatformGateway/src/main/java/com/example/platformgateway/service/UserService.java
@@ -1,18 +1,20 @@
 package com.example.platformgateway.service;
 
+import com.example.platformgateway.exception.BadRequestException;
 import com.example.platformgateway.exception.NonAuthenticatedAccessException;
-import com.example.platformgateway.model.dto.JwtPayloadDTO;
-import com.example.platformgateway.model.dto.UserSummaryDTO;
-import com.example.platformgateway.model.dto.UserSummaryResponseDTO;
+import com.example.platformgateway.model.dto.*;
 import com.example.platformgateway.model.entity.User;
 import com.example.platformgateway.model.enums.Role;
+import com.example.platformgateway.repository.CompanyRepository;
 import com.example.platformgateway.repository.UserRepository;
+import com.example.platformgateway.utils.BcryptUtility;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,9 +23,13 @@ import java.util.Optional;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final CompanyRepository companyRepository;
+  private final EmailService emailService;
 
-  public UserService(UserRepository userRepository){
+  public UserService(UserRepository userRepository, CompanyRepository companyRepository , EmailService emailService){
     this.userRepository = userRepository;
+    this.companyRepository = companyRepository;
+    this.emailService = emailService;
   }
 
   public UserSummaryResponseDTO getAllUsers(int page, String firstName, String lastName, Role role){
@@ -43,5 +49,28 @@ public class UserService {
                     user.getRole()
             )).toList();
     return new UserSummaryResponseDTO( userPage.getTotalElements() , userPage.getTotalPages() , users );
+  }
+
+  @Transactional
+  public CreateUserResponseDTO createUser(CreateUserRequestDTO createUserRequestDTO){
+    String userPassword = BcryptUtility.generateRandomPassword();
+    JwtPayloadDTO authContext = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+            .map(Authentication::getPrincipal)
+            .filter(JwtPayloadDTO.class::isInstance)
+            .map(JwtPayloadDTO.class::cast)
+            .orElseThrow(() -> new NonAuthenticatedAccessException("Access Denied , Non Authenticated"));
+
+    if(createUserRequestDTO.role().equals(Role.SUPER_RH)) throw new BadRequestException("Cannot create a user with SUPER_RH role");
+    User newUser = User.builder()
+            .firstName(createUserRequestDTO.firstName())
+            .lastName(createUserRequestDTO.lastName())
+            .email(createUserRequestDTO.email())
+            .role(createUserRequestDTO.role())
+            .password(BcryptUtility.hashPassword(userPassword))
+            .company(companyRepository.findById(authContext.companyId()).orElseThrow(() -> new BadRequestException("Company not found")))
+            .build();
+    User savedUser = userRepository.save(newUser);
+    emailService.sendEmail(emailService.buildEmailDetails(savedUser.getEmail(), userPassword, true));
+    return new CreateUserResponseDTO(savedUser.getId());
   }
 }
